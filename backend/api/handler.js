55eadcaf32d5a8af886d229462c3fea04f16f526
salwamhrn11 <salwamaharani920@gmail.com>
2024-06-02 12:26:35 +0700

    buat deploy

--- backend/api/handler.js
+++ backend/api/handler.js
@@ -1,4 +1,4 @@
 const serverless = require('serverless-http');
 const app = require('./index');
 
-module.exports.handler = serverless(app);
+module.exports = serverless(app);
--- backend/api/index.js
+++ backend/api/index.js
@@ -2,26 +2,49 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
 const app = express();
 const userRoutes = require('../routes/userRoutes');
 const medicalRecordRoutes = require('../routes/medicalRecordRoutes');
 const articleRoutes = require('../routes/articleRoutes');
 const port = 8080;
-
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(`<main>
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap');
+    main{
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      align-items: center;
+      min-height: 100vh;
+      font-family: 'Poppins', sans-serif;
+      color: #1B2430;
+    }
+    img {
+      width: 200px;
+    }
+    p, h2 {
+      text-align: center;
+    }
+  </style>
+  <div>
+    <h2>DiCheck Backend</h2>
+    <p>Ver 1.0</p>
+  </div>
+</main>`);
+});
 
 app.use("/api/user", userRoutes);
 app.use("/api/record", medicalRecordRoutes);
 app.use("/api/article", articleRoutes);
-
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   console.log(`Received ${req.method} request to ${req.url}`);
   next();
 });
-
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -35,5 +58,5 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-module.exports = app; // Tambahkan ini
+  
+  module.exports = app;
